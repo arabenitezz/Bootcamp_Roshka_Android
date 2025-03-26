93c73b2388bba0cd8b28e514716ef6f81fea828e
Arami Benitez <abenitezr18@gmail.com>
2025-03-26 11:31:34 -0300

    again

--- Pokemon_Project/app/src/main/java/com/example/pokemon_project/PokemonApp.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pokemon_project
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-
-@HiltAndroidApp
-class PokemonApp: Application() {
-}
--- Pokemon_Project/app/src/main/java/com/example/pokemon_project/utils/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pokemon_project.utils
-
-object Constants {
-
-    const val BASE_URL = "https://pokeapi.co/api/v2/pokemon/?limit=151"
-}
